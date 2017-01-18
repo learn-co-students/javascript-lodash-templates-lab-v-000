60396c814032039d58f41f233c4f9d6b089c5008
Brianna Titone <briannactitone@gmail.com>
2017-01-18 04:34:38 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+var page-template = '<div id="page"><div id="sidebar"></div<div id="post"></div>';
+var post-template = '<div id="article"><h id="header"><%= header %></h><footer><%= footer %></footer></div>';
+var comments-template = '<div id="comments"><%= comments %></div>';
+var comment-template  =  <'div id="comment"></div><footer><%= name %></footer>';
+
+function createPost(){
+  var page = document.getElementById("page-template").value;
+  var post   = document.getElementById("post-template").value;
+}
+function postComment(){
+  
+}
