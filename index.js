9806b485ef41ab391f718fc162ee07aa5019376d
Christian Whitesides <whitesidescr@gmail.com>
2017-03-28 15:58:16 -0700

    Done.

--- index.js
+++ index.js
@@ -1,19 +1,33 @@
 function createPost() {
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML)
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML)
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML)
+
   var title = document.getElementById("postTitle").value
   var content = document.getElementById("postContent").value
   var author = document.getElementById("postAuthor").value
 
-  var postTemplate = document.getElementById("post-template").innerHTML
-  var templatePn = _.template(postTemplate)
+  if (document.getElementById("post") === null) {
+    document.getElementsByTagName("main")[0].innerHTML += pageTemplate()
+  }
 
-  var pageTemplate = document.getElementById("page-template").innerHTML
+  var postHTML = postTemplate({'title': title, 'content': content, 'author': author})
+  var post = document.getElementById("post")
+  var commentsSection = commentsTemplate()
 
-  var postTemplateHTML = templatePn({'title': title, 'content': content, 'author': author})
-
-  postDiv.innerHTML += postTemplateHTML
+  post.innerHTML += postHTML
+  post.getElementsByTagName("footer")[0].innerHTML += commentsSection
 }
 
 
 function postComment() {
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML)
 
+  var comment = document.getElementById("commentText").value
+  var commenter = document.getElementById("commenterName").value
+
+  var commentsDiv = document.getElementById("comments")
+  var commentHTML = commentTemplate({'comment': comment, 'commenter': commenter})
+
+  commentsDiv.innerHTML += commentHTML
 }
