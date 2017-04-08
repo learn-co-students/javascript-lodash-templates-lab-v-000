9881a3084d600260fd311df3c6f056c489086acd
Ivan Pilot <ivanpilot@gmail.com>
2017-04-08 17:11:53 +0000

    Done.

--- index.js
+++ index.js
@@ -9,9 +9,9 @@ function createPost(){
 
   var postTemplateHTML = postTemplate({'title': title, 'content': content, 'author': author});
 
-  document.getElementsByTagName("main").innerHTML += pageTemplate();
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
   var postElement = document.getElementById("post");
-  postElement.innerHTML += templatePostHTML
+  postElement.innerHTML += postTemplateHTML
 }
 
 function postComment(){
