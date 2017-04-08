bac85da736e8519b9e3d06b2d2f31ab7cb85de6f
Ivan Pilot <ivanpilot@gmail.com>
2017-04-08 22:38:23 +0000

    Done.

--- index.js
+++ index.js
@@ -5,15 +5,28 @@ function createPost(){
 
   var pageTemplate = _.template(document.getElementById('page-template').innerHTML);
   var postTemplate = _.template(document.getElementById('post-template').innerHTML);
-  // var templateComments = _.template(document.getElementById('comments-template').innerHTML);
+  var commentsTemplate = _.template(document.getElementById('comments-template').innerHTML);
 
   var postTemplateHTML = postTemplate({'title': title, 'content': content, 'author': author});
 
   document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
   var postElement = document.getElementById("post");
   postElement.innerHTML += postTemplateHTML
+
+  document.getElementsByTagName("main")[0].innerHTML += commentsTemplate();
 }
 
 function postComment(){
+  var commenter = document.getElementById('commentAuthor').value
+  var commentText = document.getElementById('commentContent').value
+
+  // var commentText
+  var commentTemplate = _.template(document.getElementById('comment-template').innerHTML)
+
+  var commentHTML = commentTemplate({'comment': commentText, 'commenter': commenter})
+
+  var comments = document.getElementById('comments')
+  comments.innerHTML += commentHTML
 
 }
