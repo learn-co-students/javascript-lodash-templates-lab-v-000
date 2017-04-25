a70ddc62d82aa99296fdd5fc26cd1b655d837ca1
Bob Habig <rfhab2@gmail.com>
2017-04-24 20:32:55 -0400

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-function postComment() {
-
-}
 
 function createPost() {
-  
+  // post variables
+  var postTitle = document.getElementById('postTitle').value;
+  var postContent = document.getElementById('postContent').value;
+  var postAuthor = document.getElementById('postAuthor').value;
+  // page structure templates
+  var pageTemplateFn = _.template(document.getElementById('page-template').innerHTML);
+  var postTemplateFn = _.template(document.getElementById('post-template').innerHTML);
+  // JSON objects
+  var postTemplateHTML = postTemplateFn({'title': postTitle, 'content': postContent, 'author': postAuthor});
+  // appending content
+  document.getElementsByTagName('main')[0].innerHTML += pageTemplateFn();
+  document.getElementById('posts').innerHTML += postTemplateHTML;
+}
+
+function postComment() {
+
 }
