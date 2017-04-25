586bf1a74faa35524cff73d6fedc396fdc6130c4
Bob Habig <rfhab2@gmail.com>
2017-04-24 23:05:22 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var counter = 1
 
 function createPost() {
   // post variables
@@ -7,13 +8,25 @@ function createPost() {
   // page structure templates
   var pageTemplateFn = _.template(document.getElementById('page-template').innerHTML);
   var postTemplateFn = _.template(document.getElementById('post-template').innerHTML);
+  var commentsTemplateFn = _.template(document.getElementById('comments-template').innerHTML);
   // JSON objects
   var postTemplateHTML = postTemplateFn({'title': postTitle, 'content': postContent, 'author': postAuthor});
   // appending content
   document.getElementsByTagName('main')[0].innerHTML += pageTemplateFn();
-  document.getElementById('posts').innerHTML += postTemplateHTML;
+  document.getElementById('post').innerHTML += postTemplateHTML;
+  debugger;
+  document.getElementById(`post-${counter}-comments`).innerHTML += commentsTemplateFn();
+  counter++;
 }
 
 function postComment() {
+  // values
+  var comment = document.getElementById('comment').value;
+  var commenter = document.getElementById('commenter').value;
+  // template functions
+  var commentTemplateFn = _.template(document.getElementById('comment-template').innerHTML);
+  // JSON
+  var commentTemplateHTML = commentTemplateFn({ 'comment': comment, 'commenter': commenter });
 
+  document.getElementById('comments').innerHTML += commentTemplateHTML;
 }
