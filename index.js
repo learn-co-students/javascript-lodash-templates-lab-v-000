048be9863ece7fde69528d2aa1126300f95fd8ca
kkterai <kkterai@gmail.com>
2017-05-23 07:03:59 -0700

    Done.

--- index.js
+++ index.js
@@ -3,11 +3,23 @@ function createPost() {
   var postTemplate = _.template(document.getElementById("post-template").innerHTML);
   var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
 
+  //Get blog values from submitted form
   var postTitle = document.getElementById("postTitle").value;
   var postAuthor = document.getElementById("postAuthor").value;
   var postBody = document.getElementById("postBody").value;
 
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
 
+  // var postSection = _.template(document.getElementById("post").innerHTML);
+
+  var blogSection = postTemplate({ 'postTitle': postTitle, 'postAuthor': postAuthor, 'postBody': postBody})
+  var commentsSection = commentsTemplate();
+  var postElement = document.getElementById("post");
+
+  postElement.innerHTML = blogSection;
+  postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+
+  // postSection.innerHTML += postTemplate({ 'postTitle': postTitle, 'postAuthor': postAuthor, 'postBody': postBody})
 }
 
 function postComment() {
