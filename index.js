1d79560199b14749a1e4aa652bf705d3f7535425
Menachem Hornbacher <me@mendel.tech>
2017-06-30 21:01:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 function createPost(){
 
   renderPage();
-
+  // renderTemplate("post-form")
+  //
   // var title = document.getElementById("title").value;
   // var body = document.getElementById("body").value;
   // var author = document.getElementById("author").value;
@@ -10,19 +11,25 @@ function createPost(){
     body = "Content",
     author = "Some Guy"
 
-  var postTemplate = document.getElementById("post-template");
+  var postTemplate = document.getElementById("post-template").innerHTML;
 
   postTemplate = _.template(postTemplate);
 
   var postTemplateHTML = postTemplate({ 'post_title': title, 'post_content': body, 'post_author': author});
+
+  document.getElementById("post").innerHTML = postTemplateHTML
 }
 
 function postComment(){
+  renderPage();
 
+  var commentTemplate = document.getElementById("comment-template").innerHTML; // note the innerHTML
+
+  commentTemplate = _.template(commentTemplate); // this is all that is needed to pass the tests
 }
 
 function renderPage(){
-  var pageTemplate = document.getElementById("page-template");
+  var pageTemplate = document.getElementById("page-template").innerHTML;
 
   pageTemplate = _.template(pageTemplate);
 
@@ -31,4 +38,12 @@ function renderPage(){
   var bodyMain = document.getElementById("body");
 
   bodyMain.innerHTML = pageTemplateHTML
+}
+
+function renderTemplate(template){
+  var template = document.getElementById(template).innerHTML
+  template = _.template(template);
+  template = template()
+  var bodyMain = document.getElementById("body");
+  bodyMain.innerHTML = template
 }
