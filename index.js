0fa09d558d1386f40a9d10f1812a5985703efe21
vanitanaidu <vanita112003@gmail.com>
2017-07-23 00:08:40 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,18 @@
+function createPost(){
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+
+  var postTitle = document.getElementById("postTitle").value;
+  var postAuthor = document.getElementById("postAuthor").value;
+  var post = document.getElementById("postBody").value;
+}
+
+
+function postComment() {
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
+  var commenterName = document.getElementById("commenter").value;
+  var commentText = document.getElementById("commentText").value;
+
+}
