f0f3dfdc567fe52abcd0e8790ce62a85d69a118a
github username <github email address>
2017-08-09 23:20:09 -0500

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function postComment() {
+function apostComment() {
 	// Grabs the values of teh Comment Form
   var commenter = document.getElementById("commenterName").value;
   var comment = document.getElementById("commentText").value;
@@ -24,31 +24,88 @@ function postComment() {
 }
 
 function createPost() {
-	// Grabs the values of the Post Form
+    // Grabs the values of the Post Form
   var title = document.getElementById("postTitle").value;
   var author = document.getElementById("author").value;
-  var post = document.getElementById("postBody").value;
+  var body = document.getElementById("postBody").value;
 
-  	// Grabs the various templates in a string form
+  // Grabs the various templates in a string form
   var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   var postTemplate = _.template(document.getElementById("post-template").innerHTML);
-  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+  var commentTemplate = _.template(document.getElementById("comments-template").innerHTML);
 
-  	// Adds a new pageTemplate to the main document.
+  // Adds a new pageTemplate to the main document.
   document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
 
-    	// Grabs the div of all current posts
+  // Grabs the div of all current posts
   var posts = document.getElementById("post");
 
-  	// Creates a new Post
-  var NewPost = postTemplate({ 'title': title, 'author': author, 'body': post });
+  // Creates a new Post
+  var newPost = postTemplate({"title": title, "body": body, "author": author});
 
-  	// Adds the newly created posts to our div posts.
-  posts.innerHTML += NewPost;
+  // Adds the newly created posts to our div posts.
+  posts.innerHTML += newPost;
 
-  	// Creates the Comment Form
-  var comments = commentsTemplate();
-
-  	// Adds the Comment Form to the footer of our newly created post.
-  posts.getElementsByTagName("footer")[0].innerHTML += comments
+  // Adds the Comment Form to the footer of our newly created post.
+  posts.getElementsByTagName("footer")[0].innerHTML += commentTemplate()
 }
+
+function postComment() {
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
+  var newComment = commentTemplate({"commenter": commenter, "comment": comment});
+
+  var comments = document.getElementById("comments");
+
+  comments.innerHTML += newComment
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
