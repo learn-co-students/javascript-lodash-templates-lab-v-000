4252e3d6f121a4b4f2ac1a2dd61b6b20d319baa2
wook kim <wooke2e@gmail.com>
2017-08-18 14:11:12 +0000

    Done.

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
 function createPost() {
   console.log("works")
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+
   var ctitle = document.getElementById("commentTitle").value;
   var cbody = document.getElementById("commentBody").value;
   var cauthor = document.getElementById("commentAuthor").value;
-  var pageTemplate = document.getElementById("page-template").innerHTML;
-  var templateFn = _.template(pageTemplate);
-  var postsDiv = document.getElementById("posts");
-  var templateHTML = templateFn({ 'ctitle': ctitle, 'cbody': cbody, 'cauthor': cauthor });
-  postsDiv.innerHTML += templateHTML;
+
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+  var blogSection = postTemplate({ 'ctitle': ctitle, 'cbody': cbody, 'cauthor': cauthor });
+  var commentsSection = commentsTemplate();
+  var postsDiv = document.getElementById("post");
+
+  postsDiv.innerHTML = blogSection;
+  postsDiv.getElementsByTagName("footer")[0].innerHTML = commentsSection;
 }
 
 function postComment() {
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
 
+  var commentText = document.getElementById("commentText").value;
+  var commenterName = document.getElementById("commenter").value;
+
+  var commentsSection = document.getElementById("comments");
+  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
 }
