c7426278d3e440e59106befddf98a3fb30fb3e1e
Luisa Scavo <luisascavo@gmail.com>
2017-08-24 01:13:06 +0000

    Done.

--- index.js
+++ index.js
@@ -3,25 +3,23 @@ function createPost() {
   var author = document.getElementById("postAuthor").value;
   var body = document.getElementById("postBody").value;
 
-  var pageTemplate = _.template(pageTemplate).innerHTML;
-  var postTemplate =  _.template(postTemplate).innerHTML;
-  var commentsTemplate = _.template(commentsTemplate).innerHTML;
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var postTemplate =  _.template(document.getElementById("post-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
 
   document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
 
-  var postHTML = postTemplate({"title": title, "body": body, "author": author});
-
-  postsDiv = document.getElementById("posts");
-  postsDiv.append(postHTML);
+  document.getElementById("post").innerHTML += postTemplate({"title": title, "body": body, "author": author});
+  document.getElementsByTagName("footer")[0].innerHTML += commentsTemplate();
 }
 
 function postComment() {
   var commenter = document.getElementById("commenterName").value;
   var comment = document.getElementById("commentText").value;
 
-  var commentTemplate = document.getElementById("commentTemplate");
+  var commentTemplate = document.getElementById("comment-template");
 
-  var templateFn = _.template(commentTemplate);
+  var templateFn = _.template(commentTemplate.innerHTML);
 
   var commentsDiv = document.getElementById("comments");
 
