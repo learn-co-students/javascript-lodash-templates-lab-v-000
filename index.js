add8bde68a9393d88912b67bcce89dc4dbcd7ded
Pamela Bortnick <pbortnick@gmail.com>
2017-08-29 16:34:15 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,12 @@
+function createPost() {
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML)
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML)
+
+  var postTitle = document.getElementById('postTitle').value
+  var postAuthor = document.getElementById('postAuthor').value
+  var postBody = document.getElementById('postBody').value
+}
+
+function postComment() {
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML)
+}
