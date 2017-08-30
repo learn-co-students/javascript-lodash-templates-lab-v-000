ed1b78f423430bfb6c3d9f82fa529ccfec9eb352
jessica fletcher <cara.kane@gmail.com>
2017-08-30 17:05:58 +0000

    Done.

--- index.js
+++ index.js
@@ -14,5 +14,15 @@ function postComment() {
 }
 
 function createPost() {
-
+  var title = document.getElementById("postTitle").value;
+  var body = document.getElementById("postBody").value;
+  var author = document.getElementById("postAuthor").value;
+  var postTemplate = document.getElementById("post-template").innerHTML;
+  //create template function
+  var templateFn = _.template(postTemplate);
+  var postDiv = document.getElementById("post");
+  //execute template function with JSON object for the interpolated values
+  var templateHTML = templateFn({ 'title': title, 'body': body, 'author': author });
+  //append rather than replace!
+  postDiv.innerHTML += templateHTML;
 }
