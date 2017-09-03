2b1c9b126bfa5ef80a38981588e311026b4fbd3b
Joe <jsbenning4@gmail.com>
2017-09-02 23:34:04 -0400

    Done.

--- index.js
+++ index.js
@@ -1,47 +1,53 @@
-function init() {
-  
-var commentForm = document.getElementById('comment-form');
-commentForm.style.display = "none";
-
-  function createPost() {
-    
-    var postForm = document.getElementById('post-form');
-    var postTitle = document.getElementById('post-title').value;
-    var postBody = document.getElementById('post-body').value;
-    var postAuthor = document.getElementById('post-author').value;
-    var pageTemplate = document.getElementById('page-template').innerHTML;
-    var postTemplate = document.getElementById('post-template').innerHTML;
-    var commentForm = document.getElementById('comment-form');
-
-    var pageTemplateFn = _.template(pageTemplate);
-    var postTemplateFn = _.template(postTemplate);
-    var pageHTML = pageTemplateFn();
-    var postHTML = postTemplateFn({'postTitle': postTitle, 'postBody': postBody, 'postAuthor': postAuthor});
-    
-    var loadingPoint = document.getElementById('loading-point'); // added templates
-    loadingPoint.innerHTML += pageHTML;
-    var postPoint = document.getElementById("post");
-    postPoint.innerHTML += postHTML;
-    postForm.style.display = "none";
-    var commentFormArea = document.getElementById('comment-form-area');
-    // var commentTemplate = document.getElementById('comment-form').innerHTML;
-    // var commentTemplateFn = _.template(commentTemplate);
-commentForm.style.display = "block";
-    // commentFormArea.innerHTML += commentForm;  
-  }
-
-  function postComment() {
-
-    var commentsArea = document.getElementById('comments-area');
-    var commenter = document.getElementById('commenter-name').value;
-    var comment = document.getElementById('comment').value;
-    var commentTemplate = document.getElementById('comment-template').innerHTML;
-    var commentTemplateFn = _.template(commentTemplate);
-    var commentHTML = commentTemplateFn({'comment': comment,'commenter': commenter});
-    commentsArea.innerHTML += commentHTML;
-
-  }
+function init() { 
+  var commentForm = document.getElementById('comment-form');
+  commentForm.style.display = "none";
 }
+
+
+function createPost() {
+  
+  var postForm = document.getElementById('post-form');
+  var postTitle = document.getElementById('post-title').value;
+  var postBody = document.getElementById('post-body').value;
+  var postAuthor = document.getElementById('post-author').value;
+  var pageTemplate = document.getElementById('page-template').innerHTML;
+  var postTemplate = document.getElementById('post-template').innerHTML;
+  var commentForm = document.getElementById('comment-form');
+
+  var pageTemplateFn = _.template(pageTemplate);
+  var postTemplateFn = _.template(postTemplate);
+  var pageHTML = pageTemplateFn();
+  var postHTML = postTemplateFn({'postTitle': postTitle, 'postBody': postBody, 'postAuthor': postAuthor});
+  
+  var loadingPoint = document.getElementById('loading-point'); // added templates
+  loadingPoint.innerHTML += pageHTML;
+  var postPoint = document.getElementById("post");
+  postPoint.innerHTML += postHTML;
+  postForm.style.display = "none";
+  var commentFormArea = document.getElementById('comment-form-area');
+  commentForm.style.display = "block"; 
+}
+
+function postComment() {
+
+  var commentsArea = document.getElementById('comments-area');
+
+  var commenter = document.getElementById('commenter-name').value;
+  var comment = document.getElementById('comment').value;
+
+  var commentTemplate = document.getElementById('comment-template').innerHTML;
+  var commentTemplateFn = _.template(commentTemplate);
+  var commentHTML = commentTemplateFn({'comment': comment,'commenter': commenter});
+
+  commentsArea.innerHTML += commentHTML;
+
+  var innerCommenter = document.getElementById('posted-by');
+  var innerComment = document.getElementById('comment-body')
+  innerCommenter = "";
+  innerComment = "";
+
+}
+
 
 window.onload= init;
 
