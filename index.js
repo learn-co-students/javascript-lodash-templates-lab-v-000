2523d285369f07db7eb66b2d37414a06959e41e5
curlywallst <nnoyes@comcast.net>
2017-09-11 08:58:18 -0400

    Done.

--- index.js
+++ index.js
@@ -5,32 +5,35 @@ function createPost() {
   //insert blog into "blog" div in this format:
   //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
   var postTemplate = document.getElementById("post-template").innerHTML;
-  var pageTemplate = document.getElementById('page-template').innerHTML
+  var pageTemplate = document.getElementById('page-template').innerHTML;
+  var commentsTemplate = document.getElementById("comments-template").innerHTML;
   //create template string
   //create template function
   var templateFn = _.template(postTemplate);
-  var postDiv = document.getElementById("post-form");
+  var postFormDiv = document.getElementById("post-form");
   var templatePageFn = _.template(pageTemplate);
-  var pageDiv = document.getElementById('page')
-
-
+  var templateCommentsFn = _.template(commentsTemplate)
   //execute template function with JSON object for the interpolated values
   var templateHTML = templateFn({ 'title': title, 'body': body, 'author': author });
   var templatePageHTML = templatePageFn()
+  var templateCommentsHTML = templateCommentsFn()
 
+  postFormDiv.innerHTML += templatePageHTML;
+
+  var pageDiv = document.querySelector("#post");
+  pageDiv.innerHTML += templateHTML;
+
+  var commentsDiv = document.getElementById('comments-form')
+  commentsDiv.innerHTML += templateCommentsHTML
   //append rather than replace!
-  postDiv.innerHTML += templateHTML;
-  pageDiv.innerHTML = templatePageHTML;
-  
+  //var pageDiv = document.querySelector("#pageId")
+  // postDiv.innerHTML += templatePageHTML;
 }
 
 function postComment() {
   var commenter = document.getElementById("commenterName").value;
   var comment = document.getElementById("commentText").value;
   //insert comment into "comments" div in this format:
-  //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
-  var commentsTemplate = document.getElementById("comments-template").innerHTML;
-  //create template string
   var commentTemplate = document.getElementById("comment-template").innerHTML;
   //create template function
   var templateFn = _.template(commentTemplate);
