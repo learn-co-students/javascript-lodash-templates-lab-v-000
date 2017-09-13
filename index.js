97699b128fa1a37051ddc9806505f4418bd7ad00
dustin <dandersonsoftware@outlook.com>
2017-09-13 13:20:29 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,28 @@
+function createPost() {
+  var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+  var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+  var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+
+  var postTitle = document.getElementById("postTitle").value;
+  var postAuthor = document.getElementById("postAuthor").value;
+  var post = document.getElementById("postBody").value;
+
+  document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+  var article = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor })
+  var commentsSection = commentsTemplate();
+  var postItem = document.getElementById("post");
+
+  postItem.innerHTML = article;
+  postItem.getElementsByTagName("footer")[0].innerHTML = commentsSection;
+}
+
+function postComment() {
+  var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
+
+  var commentText = document.getElementById("commentText").value;
+  var commenterName = document.getElementById("commenter").value;
+
+  var commentsSection = document.getElementById("comments");
+  commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText })
+}
