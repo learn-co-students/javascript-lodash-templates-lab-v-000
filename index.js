6421657dfe1db488cd51ebd963c5a3fc121e7a71
ChristopherJamesN <nady.christopher@gmail.com>
2017-10-04 14:23:04 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,48 @@
+// index.js
+function createPost() {
+  var postTitle = document.getElementById("postTitle").value;
+  var poster = document.getElementById("posterName").value;
+  var postBody = document.getElementById("postText").value;
+
+  //insert comment into "comments" div in this format:
+  //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
+
+  //create template string - THIS IS THE ONLY LINE WE HAVE TO CHANGE
+  //var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
+  var postTemplate = document.getElementById("post-template").innerHTML;
+  var pageTemplate = document.getElementById("page-template").innerHTML;
+
+  //create template function
+  var templateFn = _.template(postTemplate);
+  var pageTemplateFn = _.template(pageTemplate);
+
+  //execute template function with JSON object for the interpolated values
+  var templateHTML = templateFn({ 'postTitle': postTitle, 'postBody': postBody, 'poster': poster });
+  var pageTemplatHTML = pageTemplateFn({'post': templateHTML})
+
+  //append rather than replace!
+  //pageTemplate += templateHTML;
+}
+
+function postComment() {
+  var commenter = document.getElementById("commenterName").value;
+  var comment = document.getElementById("commentText").value;
+
+  //insert comment into "comments" div in this format:
+  //<div class="comment"><p>comment</p><p>Posted By: <span class="commenter">commenter</span></p></div>
+
+  //create template string - THIS IS THE ONLY LINE WE HAVE TO CHANGE
+  //var commentTemplate = '<div class="comment"><p><%= comment %></p><p>Posted By: <span class="commenter"><%= commenter %></span></p></div>';
+  var commentTemplate = document.getElementById("comment-template").innerHTML;
+
+  //create template function
+  var templateFn = _.template(commentTemplate);
+
+  var commentsDiv = document.getElementById("comments");
+
+  //execute template function with JSON object for the interpolated values
+  var templateHTML = templateFn({ 'comment': comment, 'commenter': commenter });
+
+  //append rather than replace!
+  commentsDiv.innerHTML += templateHTML;
+}
