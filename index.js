3105a2b63c772785cf41633f268f88786963e996
Alexandra <github email address>
2017-12-04 08:16:50 -0800

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 function createPost() {
-    // create template functions
-    var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
-    var postTemplate = _.template(document.getElementById("post-template").innerHTML);
-    var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
 
-    // get blog values
+    let pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+    let postTemplate = _.template(document.getElementById("post-template").innerHTML);
+    let commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
+
     var postTitle = document.getElementById("postTitle").value;
     var postAuthor = document.getElementById("postAuthor").value;
     var post = document.getElementById("postBody").value;
