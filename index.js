25333edb5e72ddc9323209cb26feb4a6d7410a07
Alexandra <github email address>
2017-12-03 20:32:08 -0800

    Done.

--- index.js
+++ index.js
@@ -1,51 +1,30 @@
-function loadPageTemplate() {
-    const pageTemplate = document.getElementById("page-template").innerHTML
-    const templateFn = _.template(pageTemplate)
-    const main = document.getElementById("main")
-    const templateHTML = templateFn()
-    main.innerHTML += templateHTML
-    loadCommentsTemplate()
-}
+function createPost() {
+    // create template functions
+    var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
+    var postTemplate = _.template(document.getElementById("post-template").innerHTML);
+    var commentsTemplate = _.template(document.getElementById("comments-template").innerHTML);
 
-function loadPostTemplate() {
-    const postTemplate = document.getElementById("post-template").innerHTML
-    const templateFn = _.template(postTemplate)
-    const main = document.getElementById("post")
-    const templateHTML = templateFn()
-    main.innerHTML += templateHTML
-}
+    // get blog values
+    var postTitle = document.getElementById("postTitle").value;
+    var postAuthor = document.getElementById("postAuthor").value;
+    var post = document.getElementById("postBody").value;
 
-function loadCommentsTemplate() {
-    const commentsTemplate = document.getElementById("comments-template").innerHTML
-    const templateFn = _.template(commentsTemplate)
-    const main = document.getElementById("comments-box")
-    const templateHTML = templateFn()
-    main.innerHTML += templateHTML
+    document.getElementsByTagName("main")[0].innerHTML += pageTemplate();
+
+    var blogSection = postTemplate({ 'title': postTitle, 'body': post, 'poster': postAuthor });
+    var commentsSection = commentsTemplate();
+    var postElement = document.getElementById("post");
+
+    postElement.innerHTML = blogSection;
+    postElement.getElementsByTagName("footer")[0].innerHTML = commentsSection;
 }
 
 function postComment() {
-    let commenter = document.getElementById("commenterName").value
-    let comment = document.getElementById("commentText").value
+    var commentTemplate = _.template(document.getElementById("comment-template").innerHTML);
 
-    let commentTemplate = document.getElementById("comment-template").innerHTML
+    var commentText = document.getElementById("commentText").value;
+    var commenterName = document.getElementById("commenter").value;
 
-    const templateFn = _.template(commentTemplate)
-
-    let commentsDiv = document.getElementById("comments")
-    let templateHTML = templateFn({ "comment": comment, "commenter": commenter })
-    commentsDiv.innerHTML += templateHTML
-}
-
-function createPost() {
-    const author = document.getElementById("postAuthor").value
-    const title = document.getElementById("postTitle").value
-    const content = document.getElementById("postContent").value
-
-    let postTemplate = document.getElementById("post-template").innerHTML
-
-    const templateFn = _.template(postTemplate)
-
-    let postDiv = document.getElementById("post")
-    let templateHTML = templateFn({ "author": author, "content": content, "title": title })
-    postDiv.innerHTML += templateHTML
+    var commentsSection = document.getElementById("comments");
+    commentsSection.innerHTML += commentTemplate({ 'commenter': commenterName, 'comment': commentText });
 }
