67d50a96bb6b749977fd2e09fd15be3e76ae3b5c
BuffaloRob <peckham.rob@gmail.com>
2018-03-05 10:36:03 -0500

    fixed typo in index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-fucntion createPost() {
+function createPost() {
   //create template functions
   var pageTemplate = _.template(document.getElementById("page-template").innerHTML);
   var postTemplate = _.template(document.getElementById("post-template").innerHTML);
